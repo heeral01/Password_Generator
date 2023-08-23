0d93bf8d875a6d9fe55cb51a14c1fb99b19764bb
heeral01 <117432070+heeral01@users.noreply.github.com>
2023-08-23 22:08:51 +0530

    Create PasswordGenerator.java
    This is a main file for our project

---
+++ PasswordGenerator.java
@@ -0,0 +1,16 @@
+package passwordgenerator;
+
+/**
+ *
+ * @author Heeral Gour
+ */
+public class PasswordGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
